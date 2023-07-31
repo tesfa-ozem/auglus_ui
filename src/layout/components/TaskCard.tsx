5dad65dabae0da1e8264236387eaea94c31de577
tesfa-ozem <alphatesfa789@gmail.com>
2023-07-31 10:21:23 +0300

    Display tasks

---
+++ src/layout/components/TaskCard.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Chip,
+  Button,
+  Box,
+  styled,
+} from "@mui/material";
+
+// Define custom styles for the status chip using MUI's styled API
+const StatusChip = styled(Chip)(({ theme }) => ({
+  color: theme.palette.getContrastText(theme.palette.warning.main),
+  backgroundColor: theme.palette.warning.main,
+}));
+
+const TaskCard = ({ taskId,title, priority, status, assignee, startTask, endTask}) => {
+  return (
+    <Card variant="outlined">
+      <CardContent>
+        <Typography variant="h5" component="div">
+          {title}
+        </Typography>
+        {/* <Typography sx={{ mt: 1 }} color="text.secondary">
+          Skills Required:
+        </Typography> */}
+        {/* <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+          {skillsRequired.map((skill) => (
+            <Chip key={skill} label={skill} color="primary" sx={{ m: 0.5 }} />
+          ))}
+        </Box> */}
+        <Typography sx={{ mt: 1 }} color="text.secondary">
+          Priority: {priority}
+        </Typography>
+        <Box sx={{ mt: 2,display: "flex", justifyContent: "space-between", }}>
+          
+          {status=="Completed"&&<Chip color="success" label={status} />}
+          {status=="Assigned"&&<Chip color="info" label={status} />}
+          {assignee&&<Typography variant="h5" component="div">{assignee}</Typography>}
+        </Box>
+        <Box sx={{ display: "flex", justifyContent: "space-between", mt: 2 }}>
+          <Button variant="contained" disabled={status!="New"} color="success" onClick={()=>startTask(taskId)}>
+            Start
+          </Button>
+          <Button variant="contained" disabled={status=="Completed"} color="error" onClick={()=>endTask(taskId)}>
+            End
+          </Button>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default TaskCard;
--- src/routes/dashboard/Dashboard.tsx
+++ src/routes/dashboard/Dashboard.tsx
@@ -1,98 +1,189 @@
-import { Box, Container, Typography, Grid } from '@mui/material';
-import { styled } from '@mui/system';
-import {
-  KanbanComponent,
-  ColumnsDirective,
-  ColumnDirective,
-} from '@syncfusion/ej2-react-kanban';
+import { Box } from '@mui/material';
+import { useState, useEffect } from 'react';
 import axiosInstance from '../../common/http';
-import { useEffect, useState } from 'react';
-import KanbanCard from '../../layout/components/KanbanCard';
-
-const ExpenseCard = styled('div')(() => ({
-  backgroundColor: 'white',
-  padding: '1.5rem',
-  borderRadius: '8px',
-}));
+import TaskCard from '../../layout/components/TaskCard';
+import {
+  Autocomplete,
+  TextField,
+  Button,
+  Container,
+  Grid,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+} from '@mui/material';
+import { useForm, Controller } from 'react-hook-form';
 
 const DashboardPage = () => {
-  const [tasks, setTasks] = useState([]);
-  const [kanbanData, setKanbanDat] = useState();
+  const [tasks, setTasks] = useState<any[]>([]);
+  const [skill, setSkill] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+  const existingSkills = ['React', 'JavaScript', 'HTML', 'CSS'];
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    // Process form data or send it to the server
+    console.log(data);
+  };
 
   const getTasks = async () => {
     try {
+      setLoading(true);
       const response = await axiosInstance.get('/task/userTasks');
       let response_data = response.data;
       setTasks(response_data);
+      setLoading(false);
     } catch (e) {
+      setLoading(false);
       console.log(e);
     }
   };
-  const flatenData = () => {
-    let data = tasks.map((task) => ({
-      id: task.id,
-      key: task.task.status,
-      title: task.task.name,
-      priority: task.task.priority,
-    }));
-    return data;
-  };
 
+  const startTask = async (taskId:number) => {
+    try{
+      setLoading(true);
+      const response = await axiosInstance.patch(`/task/${taskId}/start`);
+      getTasks();
+      let response_data = response.data;
+      setLoading(false);
+    } catch (e){
+      setLoading(false);
+      console.log(e);
+    }
+  }
+
+  const endTask = async (taskId:number) => {
+    try{
+      setLoading(true);
+      const response = await axiosInstance.patch(`/task/${taskId}/end`);
+      getTasks();
+      let response_data = response.data;
+      setLoading(false);
+    } catch (e){
+      setLoading(false);
+      console.log(e);
+    }
+  }
+  
   useEffect(() => {
     getTasks();
   }, []);
 
-  useEffect(() => {
-    setKanbanDat(flatenData());
-  }, [tasks]);
 
   return (
-    <Box
-      sx={{
-        minHeight: '100vh',
-      }}
-    >
-      <Container maxWidth="lg">
-        {/* <Typography variant="h4" component="h1" align="center" gutterBottom>
-          Expense Tracker Dashboard
-        </Typography> */}
+    <>
+      <Box>
+        <Grid container spacing={2} alignContent='baseline'>
+          <Grid item xs={3}>
+            <Container maxWidth="sm">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    Create a Task
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Controller
+                      name="Name"
+                      control={control}
+                      defaultValue=""
+                      rules={{ required: 'Task name is required' }}
+                      render={({ field }) => (
+                        <TextField
+                          {...field}
+                          fullWidth
+                          label="Name"
+                          error={Boolean(errors.name)}
+                          helperText={errors.name && errors.name.message}
+                        />
+                      )}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FormControl fullWidth>
+                      <InputLabel>Priority</InputLabel>
+                      <Controller
+                        name="priority"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => (
+                          <Select {...field}>
+                            <MenuItem value={1}>Low</MenuItem>
+                            <MenuItem value={2}>Medium</MenuItem>
+                            <MenuItem value={3}>High</MenuItem>
+                          </Select>
+                        )}
+                      />
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Controller
+                      name="skills"
+                      control={control}
+                      defaultValue={[]}
+                      render={({ field }) => (
+                        <Autocomplete
+                          {...field}
+                          multiple
+                          freeSolo
+                          options={existingSkills}
+                          renderInput={(params) => (
+                            <TextField
+                              {...params}
+                              fullWidth
+                              label="Skills"
+                              error={Boolean(errors.skills)}
+                              helperText={
+                                errors.skills && 'Please select or add skills'
+                              }
+                            />
+                          )}
+                          isOptionEqualToValue={(option, value) =>
+                            option === value || existingSkills.includes(value)
+                          }
+                        />
+                      )}
+                    />
+                  </Grid>
 
-        <Grid container alignItems="flex-start">
-          <Grid item xs={12} md={12} lg={12}>
-           
-            <KanbanComponent
-              id="id" 
-              keyField="key"
-              dataSource={kanbanData}
-              cardSettings={{
-                headerField: 'Title',
-                template: KanbanCard.bind(this),
-                // selectionType: 'Multiple',
-              }}
-            >
-              <ColumnsDirective>
-                <ColumnDirective
-                  headerText="Assigned"
-                  keyField="Assigned"
-                  template={KanbanCard.bind(this)}
-                />
-                <ColumnDirective
-                  headerText="In Progress"
-                  keyField="InProgress"
-                  template={KanbanCard.bind(this)}
-                />
-                <ColumnDirective
-                  headerText="Completed"
-                  keyField="Close"
-                  template={KanbanCard.bind(this)}
-                />
-              </ColumnsDirective>
-            </KanbanComponent>
-           
+                  <Grid item xs={12}>
+                    <Button type="submit" variant="contained" color="primary">
+                      Submit
+                    </Button>
+                  </Grid>
+                </Grid>
+              </form>
+            </Container>
+          </Grid>
+          <Grid  xs={9} container spacing={2}>
+            {tasks!=undefined ? (
+              tasks.map((task) => (
+                <Grid item xs={12} sm={6} md={4} key={task.id}>
+                  <TaskCard
+                    taskId={task.id}
+                    title={task.task.name}
+                    priority={task.task.priority}
+                    status={task.task.status}
+                    assignee={task.professional.first_name}
+                    startTask={startTask}
+                    endTask={endTask }
+                  />
+                </Grid>
+              ))
+            ) : (
+              <Grid item xs={12}>
+                <p>No tasks found.</p>
+              </Grid>
+            )}
           </Grid>
         </Grid>
-      </Container>
-    </Box>
+      </Box>
+    </>
   );
 };
 
